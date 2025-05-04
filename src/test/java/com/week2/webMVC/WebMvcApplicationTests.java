1c32485d5ff393bfd0b3a1f136fd64c37c11d780
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-04 09:39:26 +0530

    first commit

---
+++ src/test/java/com/week2/webMVC/WebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week2.webMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
