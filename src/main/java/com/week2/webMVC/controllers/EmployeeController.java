d7a8e525289a90d2cf3297e66bc1c4c88df29a79
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-06 02:03:23 +0530

    Dynamic URL paths + Request Body

--- src/main/java/com/week2/webMVC/controllers/EmployeeController.java
+++ src/main/java/com/week2/webMVC/controllers/EmployeeController.java
@@ -1,19 +1,33 @@
 package com.week2.webMVC.controllers;
 
 import com.week2.webMVC.dto.EmployeeDTO;
-import lombok.Data;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.xml.xpath.XPath;
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping(path = "/employee")
 public class EmployeeController {
 
-    @GetMapping(path = "/employee/{employeeId}")
+    @GetMapping(path = "/{employeeId}")
     public EmployeeDTO getMessage(@PathVariable Long employeeId){
         return new EmployeeDTO(employeeId, "Rahul Mahato", "rahul@gmail.com", 23, LocalDate.of(2023, 7, 6),true);
+    }
+
+    @GetMapping
+    public String getMessage2(@RequestParam(required = false) Integer age){
+        return "Hello, my age is : " + age;
+    }
+
+    @PostMapping
+    public String getMessage3(@RequestBody String message){
+        return message;
+    }
+
+    @PostMapping(path = "/newEmp/{employeeId}")
+    public EmployeeDTO getEmployee(@RequestBody EmployeeDTO newEmployee, @PathVariable Long employeeId){
+        newEmployee.setId(employeeId);
+        return newEmployee;
     }
 }
